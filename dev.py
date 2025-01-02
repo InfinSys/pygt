955d0ad7ad7e762bd27f3a25a9a910c061298f79
Jamon Bailey <infinity.dev@outlook.com>
2025-01-02 13:31:25 -0500

    Create dev.py

---
+++ dev.py
@@ -0,0 +1,10 @@
+
+#   IMPORTS
+from window import AppWindow
+
+
+if __name__ == "__main__":
+    print("\n| Dev Script |\n")
+
+    window: AppWindow = AppWindow(width=300, height=200)
+    window.mainloop()
