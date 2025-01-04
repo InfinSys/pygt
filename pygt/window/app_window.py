cc29a0759ad51d70229876b1895346831362444b
Jamon Bailey <infinity.dev@outlook.com>
2025-01-04 12:12:18 -0500

    Update app_window.py

--- pygt/window/app_window.py
+++ pygt/window/app_window.py
@@ -11,7 +11,7 @@ from pygt.window.controller import WindowController
 from pygt.window.event import WindowEventHandler
 from pygt.window.service import WindowServiceBroker, ServiceEndpoint
 #from pygt.window.window_view import WindowViewApparatus
-import pygt.window.service.service_keys as SvcKey
+import pygt.window.service.keys as SvcKey
 
 
 # Application DPI awareness
