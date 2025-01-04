42b7e040b337eaef2a7d8cd633b7a0a4c1721b38
Jamon Bailey <infinity.dev@outlook.com>
2025-01-04 11:14:57 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -10,11 +10,24 @@ if __name__ == "__main__":
 
     window: AppWindow = AppWindow(width=300, height=200)
 
-    window.event.bind(sequence="<Button-1>", func=lambda e: print(f"On display: {window.control.display_name()}"))
-    window.event.bind(sequence="<Button-2>", func=lambda e: print(f"Rel. mouse: {window.control.rel_mouse_coord()}"))
-    window.event.bind(sequence="<Button-3>", func=lambda e: print(f"On displays: {window.control.spanning_displays()}"))
+    #window.event.bind(
+    #    sequence="Button-1",
+    #    cmd=lambda e: print(f"On display: {window.control.display_name()}"),
+    #    identifier="id_display"
+    #)
+    #window.event.bind(
+    #    sequence="Button-2",
+    #    cmd=lambda e: print(f"Rel. mouse: {window.control.rel_mouse_coord()}"),
+    #    identifier="id_mouse_coord"
+    #)
+    #window.event.bind(
+    #    sequence="Button-3",
+    #    cmd=lambda e: print(f"On displays: {window.control.spanning_displays()}"),
+    #    identifier="id_displays"
+    #)
 
-    WindowTests.live_window_controller_test(window)
+    #WindowTests.live_window_controller_test(window)
+    WindowTests.interactive_window_event_handler_test(window)
 
     window.mainloop()
 
