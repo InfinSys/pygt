067bad2f3b4ae8efc4efc9baecaf4bd4590c2a3a
Jamon Bailey <infinity.dev@outlook.com>
2025-01-08 01:59:04 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -13,30 +13,7 @@ if __name__ == "__main__":
 
     window: Window = Window(width=300, height=200)
 
-    window.view.view.attach(
-        identifier="demo_viewport",
-        viewport_type=Viewport
-    )
-
-    #window.event.bind(
-    #    sequence="Button-1",
-    #    cmd=lambda e: print(f"On display: {window.control.display_name()}"),
-    #    identifier="id_display"
-    #)
-    #window.event.bind(
-    #    sequence="Button-2",
-    #    cmd=lambda e: print(f"Rel. mouse: {window.control.rel_mouse_coord()}"),
-    #    identifier="id_mouse_coord"
-    #)
-    #window.event.bind(
-    #    sequence="Button-3",
-    #    cmd=lambda e: print(f"On displays: {window.control.spanning_displays()}"),
-    #    identifier="id_displays"
-    #)
-
-    #WindowTests.live_window_controller_test(window)
-    #WindowTests.interactive_window_event_handler_test(window)
-    #WindowTests.live_window_services_test(window)
+    pass
 
     window.mainloop()
 
