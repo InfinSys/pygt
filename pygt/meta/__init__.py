8f89329129010e8a6b74be5465addfbc2aedac7e
Jamon Bailey <infinity.dev@outlook.com>
2025-01-08 08:35:59 -0500

    Create PyGT Metadata Package

---
+++ pygt/meta/__init__.py
@@ -0,0 +1,9 @@
+
+"""
+Python GUI Toolkit Package Metadata
+"""
+
+
+#   EXTENDED SYMBOLS
+from .package_version import VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, \
+    VERSION_TWEAK, get_pygt_version
---
+++ pygt/meta/package_version.py
@@ -0,0 +1,21 @@
+
+""" Python Graphical User Interface Toolkit Package Version Information """
+
+
+#   DEFINITIONS
+VERSION_MAJOR: int = 2
+""" PyGT version number major. """
+
+VERSION_MINOR: int = 0
+""" PyGT version number minor. """
+
+VERSION_PATCH: int = 0
+""" PyGT version number patch. """
+
+VERSION_TWEAK: int = 0
+""" PyGT version number tweak. """
+
+
+def get_pygt_version() -> str:
+    """ Returns PyGT package version number as string. """
+    return f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}.{VERSION_TWEAK}"
