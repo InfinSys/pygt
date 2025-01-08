7376f8bdd8c3760ce1c4ac460c01380db0a9474a
Jamon Bailey <infinity.dev@outlook.com>
2025-01-08 00:11:37 -0500

    Update dev.py

--- dev.py
+++ dev.py
@@ -30,6 +30,7 @@ if __name__ == "__main__":
 
     #WindowTests.live_window_controller_test(window)
     #WindowTests.interactive_window_event_handler_test(window)
+    #WindowTests.live_window_services_test(window)
 
     window.mainloop()
 
