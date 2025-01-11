8a4a04669e64e399fa2a157a5c5628d6bd5d4db1
Jamon Bailey <infinity.dev@outlook.com>
2025-01-11 03:38:35 -0500

    Create table_widget_model.py

---
+++ pygt/widget/table/table_widget_model.py
@@ -0,0 +1,21 @@
+
+""" Table Widget Model """
+
+
+#   EXTERNAL IMPORTS
+pass
+
+
+#   INTERNAL IMPORTS
+pass
+
+
+#   GLOBAL DEFINITIONS
+pass
+
+
+#   CLASSES
+class TableModel:
+    """ Table widget model. """
+    def __init__(self) -> None:
+        pass
