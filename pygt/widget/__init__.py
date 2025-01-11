fe8f77ef2230ac67ebca0f2223fc831ec4f424e6
Jamon Bailey <infinity.dev@outlook.com>
2025-01-11 03:38:53 -0500

    Update __init__.py

--- pygt/widget/__init__.py
+++ pygt/widget/__init__.py
@@ -6,11 +6,11 @@
 import pygt.widget.button as ButtonTypes
 import pygt.widget.label as LabelTypes
 import pygt.widget.entry as EntryTypes
+import pygt.widget.table as TableTypes
 
 
 #   WIDGET BOUNTIES:
 
-# > ListTable
 # > Image
 # > ScrollFrame
 # > CheckBox
