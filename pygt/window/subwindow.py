c24019e167cb423d2c0bb7be6cb0eab79250c5ae
Jamon Bailey <infinity.dev@outlook.com>
2025-01-11 23:20:24 -0500

    Update subwindow.py

--- pygt/window/subwindow.py
+++ pygt/window/subwindow.py
@@ -9,7 +9,7 @@ from tkinter import Toplevel, Tk
 #   INTERNAL IMPORTS
 from pygt.window.controller import WindowController
 from pygt.event.handler import ViewEventHandler, ExitHandler
-from pygt.window.service import WindowServiceBroker
+from pygt.window.service import WindowServiceBroker, ServiceEndpoint
 from pygt.window.view import WindowViewApparatus
 
 
@@ -20,11 +20,13 @@ pass
 #   CLASSES
 class Subwindow(Toplevel):
     """ Base application subwindow class. """
-    def __init__(self, master: Tk, service_call, width: int = None, height: int = None) -> None:
+    def __init__(self, master: Tk, service_call, width: int = None, height: int = None, **kwargs) -> None:
         super().__init__()
 
         self.__parent_window: Tk = master if issubclass(type(master), Tk) else None
         self.__service_call = service_call if callable(service_call) else None
+        self.__closure_call = kwargs.get('closure_call', None)
+        self.__closure_call = self.__closure_call if callable(self.__closure_call) else None
 
         self.__controller: WindowController = WindowController(
             window=self,
@@ -44,6 +46,14 @@ class Subwindow(Toplevel):
         )
 
         self.__exit_handler: ExitHandler = ExitHandler(final_call=self.destroy)
+
+        if self.has_managed_exit():
+            self.exit.add_exit_prerequisite(
+                identifier="master_exit_notify",
+                task=ServiceEndpoint(func=self.__closure_call)
+            )
+
+        self.__configure()
 
     @property
     def control(self) -> WindowController:
@@ -82,6 +92,11 @@ class Subwindow(Toplevel):
         """ Returns window event handler. """
         return self.__event_handler
 
+    def has_managed_exit(self) -> bool:
+        """ Returns true if subwindow is configured
+        to update status on exit. """
+        return self.__closure_call is not None
+
     def window_exit(self, prereq_override: bool = False, exit_status: int = None) -> int:
         """ Inititate window exit sequence. """
         if (prereq_override is True) and (exit_status is None):
@@ -94,3 +109,10 @@ class Subwindow(Toplevel):
         else:
             self.__exit_handler.do_graceful_exit()
             return exit_status
+
+    def __configure(self) -> None:
+        """ Prepapre application window. """
+        self.protocol("WM_DELETE_WINDOW", self.window_exit)
+
+    def __key(self) -> int:
+        return self.__hash__() + self.winfo_id()
